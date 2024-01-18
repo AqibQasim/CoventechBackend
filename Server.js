6fe4ec8940039d0835df30e74bcd467116b1b56b
AqibQasim <muhammad44aqib@gmail.com>
2024-01-18 20:48:21 +0500

    push

---
+++ Server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/CoventechDb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const ContactForm = mongoose.model('ContactForm', {
+    Name: String,
+    Email: String,
+    Subject: String,
+    message: String,
+  });
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.options('*', cors());
+
+app.use(bodyParser.json());
+
+// Route to handle form submissions
+app.post('/', async (req, res) => {
+    try {
+      const { Name, Email , Subject , message } = req.body;
+  
+      // Save the form data to MongoDB
+      const contactEntry = new ContactForm({ Name, Email , Subject , message });
+      await contactEntry.save();
+
+      console.log('Received form submission:', req.body);
+      // Send email
+      res.status(200).json({ success: true, message: 'Form data saved successfully' });
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.hostinger.com',
+      port: 465,
+      auth: {
+        user: 'aqib@co-ventech.com',
+        pass: 'Aqib@aqib1',
+      },
+    });
+
+    const mailOptions = {
+      from: 'aqib@co-ventech.com',
+      to: 'sales@co-ventech.com',
+      subject: 'Coventect Contact Form',
+      text: `Name: ${Name}\nEmail: ${Email}\nSubject: ${Subject} \nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    console.log('Email sent successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+  });
+
+  // Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
